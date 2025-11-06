6b28c1206dec55f037cf2c233969963e696e11ab
elijah <cuiyeqing960904@gmail.com>
2025-11-06 15:28:08 +0300

    built

--- vector.py
+++ vector.py
@@ -10,5 +10,28 @@ embeddings = OllamaEmbeddings(model="mxbai-embed-large")
 db_location = "./chrome_langchain_db"
 add_documents = not os.path.exists(db_location)
 
+documents = []
+ids = []
+
 if add_documents:
-    pass
+    for i, row in df.iterrows():
+        document = Document(
+            page_content=row["Title"] + " " + row["Review"],
+            metadata={"rating": row["Rating"], "date": row["Date"]},
+            id=str(i)
+        )
+        ids.append(str(i))
+        documents.append(document)
+
+vector_store = Chroma(
+    collection_name="restaurant_reviews",
+    persist_directory=db_location,
+    embedding_function=embeddings,
+)
+
+if add_documents:
+    vector_store.add_documents(documents=documents, ids=ids)
+
+retriever = vector_store.as_retriever(
+    search_kwargs={"k": 5}
+)

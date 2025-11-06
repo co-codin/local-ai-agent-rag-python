6a871766b4d8105c496a58b117e1e77f77ebeaca
elijah <cuiyeqing960904@gmail.com>
2025-11-06 14:37:24 +0300

    embeding documents to chroma

---
+++ vector.py
@@ -0,0 +1,14 @@
+from langchain_ollama import OllamaEmbeddings
+from langchain_chroma import Chroma
+from langchain_core.documents import Document
+import os
+import pandas as pd
+
+df = pd.read_csv("realistic_restaurant_reviews.csv")
+embeddings = OllamaEmbeddings(model="mxbai-embed-large")
+
+db_location = "./chrome_langchain_db"
+add_documents = not os.path.exists(db_location)
+
+if add_documents:
+    pass
